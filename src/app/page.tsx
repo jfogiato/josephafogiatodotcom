5463ca040a13c92c4bf9af4b259f10b7d75aa37e
jfogiato <joe.fogiato@gmail.com>
2025-03-01 17:31:15 -0500

    Refactor footer to separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { GithubIcon, AtSignIcon, LinkedinIcon, InstagramIcon } from "lucide-react";
+import LinkFooter from "@/components/LinkFooter";
 import { pages } from "../data/pages";
 import Link from "next/link";
 
@@ -28,7 +28,6 @@ export default function Home() {
         {/* Navigation */}
         <div className="flex flex-wrap justify-center gap-4 max-w-[80%]">
           {pages.map(({ href, icon: Icon, label }) => (
-
             <Link key={href} href={href} className="relative group">
               <Icon className="w-8 h-8 group-hover:scale-110 transition" />
               {/* Tooltip for each icon */}
@@ -36,45 +35,10 @@ export default function Home() {
                 {label}
               </span>
             </Link>
-            
           ))}
         </div>
       </div>
-
-      <footer className="grid grid-cols-2 gap-6 justify-items-center py-6 px-4 rounded-md bg-black/25 backdrop-blur-sm sm:w-[17vw] w-[25vw]">
-        {[
-          {
-            href: "https://www.linkedin.com/in/joseph-fogiato/",
-            icon: LinkedinIcon,
-            label: "Professional.",
-          },
-          {
-            href: "mailto:joseph@fogiato.com",
-            icon: AtSignIcon,
-            label: "Email.",
-          },
-          {
-            href: "https://github.com/jfogiato",
-            icon: GithubIcon,
-            label: "Code.",
-          },
-          {
-            href: "https://www.instagram.com/alrightsrsrvd/",
-            icon: InstagramIcon,
-            label: "Personal.",
-          },
-        ].map(({ href, icon: Icon, label }) => (
-          <a key={href} href={href} target="_blank" rel="noopener noreferrer" className="relative group">
-
-            <Icon className="w-6 h-6 text-white transition-transform group-hover:scale-110" />
-
-            <span className="absolute left-1/2 -translate-x-1/2 -bottom-8 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity z-10 pointer-events-none">
-              {label}
-            </span>
-
-          </a>
-        ))}
-      </footer>
+      <LinkFooter/>
     </div>
   );
 }
