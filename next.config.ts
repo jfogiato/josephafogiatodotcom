c3d53cad228653825c2bf1cffadcdc2ed8869339
jfogiato <joe.fogiato@gmail.com>
2025-03-26 09:32:36 -0400

    Reverted nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  // async rewrites() {
-  //   if (!isProd) return []; // Skip in dev
 
-  //   return [
-  //     {
-  //       source: "/",
-  //       has: [
-  //         {
-  //           type: "host",
-  //           value: "work.fogiato.com",
-  //         },
-  //       ],
-  //       destination: "/work",
-  //     },
-  //   ];
-  // },
-  // async redirects() {
-  //   if (!isProd) return []; // Skip in dev
-
-  //   return [
-  //     {
-  //       source: "/work",
-  //       destination: "https://work.fogiato.com",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
