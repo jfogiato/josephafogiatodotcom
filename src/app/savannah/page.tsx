daca8d8a995d632e5f5e478a1cacd2b34848dca1
jfogiato <joe.fogiato@gmail.com>
2025-04-19 16:19:54 -0400

    Add emily and savannah pages

---
+++ src/app/savannah/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function SavannahPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-purple-200 text-black">
+      <h1 className="text-8xl font-bold mb-10">Savannah!!</h1>
+      <Link
+        href="/emily"
+        className="px-6 py-3 bg-black text-white rounded hover:bg-gray-800 transition"
+      >
+        Go to Emily
+      </Link>
+    </div>
+  );
+}
